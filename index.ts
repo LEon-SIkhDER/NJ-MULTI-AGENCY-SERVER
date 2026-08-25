927725b499d4685a57f990a3b1931b43936e21d7
LEon-SIkhDER <leondesktop7580@gmail.com>
2026-08-25 21:24:16 +0600

    main api done

--- index.ts
+++ index.ts
@@ -14,16 +14,17 @@ dns.setServers([
     "1.1.1.1"
 ]);
 
-app.get('/', async (req: Request, res: Response) => {
-
-    res.send('Hello World!');
-});
 
 const shareLinkToImageUrl = async (shareLink: string) => {
-    const { url } = await fetch(shareLink)
+    const { url } = await fetch(shareLink, { redirect: "follow" })
     const profileImage = `https://graph.facebook.com/v23.0${new URL(url).pathname}/picture`
+    console.log(profileImage)
     return profileImage
 }
+app.get('/', async (req: Request, res: Response) => {
+    // console.log()
+    res.send('Hello World!');
+});
 
 
 const uri = process.env.URI;
@@ -54,14 +55,20 @@ const fakeDataAboutPitcher = {
     // status: "active"
 }
 
+const fakeDataAboutModerator = {
+    totalAssignedTasks: 0, lastAssignedAt: ""
+}
+
 async function run() {
     try {
         const database = client.db("Nj_Multi_Agency")
         const usersCollection = database.collection("users")
         const pitchersCollection = database.collection("pitchers")
+        const moderatorsCollection = database.collection("moderators")
+        const tasksCollection = database.collection("tasks")
 
 
-        // user ______________________
+        // user ________________________________________
         app.get("/users", async (req, res) => {
             const { search } = req.query
             let query = { role: "user" }
@@ -96,6 +103,26 @@ async function run() {
             const result = await usersCollection.insertOne(user)
             res.send(result)
         })
+        // employee ____________________________________________
+
+        app.post("/employee", async (req, res) => {
+            const employee = req.body
+            employee.createdAt = new Date()
+            employee.updatedAt = new Date()
+            employee.status = 'active'
+            await usersCollection.updateOne({ uid: employee.uid }, { $set: { role: employee.role } })
+
+
+            if (employee.role === "pitcher") {
+                const result = await pitchersCollection.insertOne(employee)
+                res.send(result)
+            }
+            if (employee.role === "moderator") {
+                const result = await moderatorsCollection.insertOne(employee)
+                res.send(result)
+            }
+
+        })
         // pitchers ___________________________
         app.get("/pitcher/:id", async (req, res) => {
             const { id } = req.params
@@ -105,7 +132,19 @@ async function run() {
             res.send(result ? { ...result, ...fakeDataAboutPitcher } : result)
         })
         app.get("/pitchers", async (req, res) => {
-            const result = await pitchersCollection.find().sort({ _id: -1 }).toArray()
+            const { search } = req.query
+            let query = {}
+            if (search) {
+                const searchQuery = {
+                    $or: [
+                        { name: { $regex: search, $options: "i" } },
+                        { email: { $regex: search, $options: "i" } }
+                    ]
+                }
+                query = { ...query, ...searchQuery }
+
+            }
+            const result = await pitchersCollection.find(query).sort({ _id: -1 }).toArray()
             const fakeResult = result.map((singleResult) => ({ ...singleResult, ...fakeDataAboutPitcher }))
             res.send(fakeResult)
         })
@@ -139,6 +178,109 @@ async function run() {
             const result = await pitchersCollection.deleteOne(uid)
             res.send(result)
         })
+        // moderators __________________________________________
+        app.get("/moderators", async (req, res) => {
+            const { search } = req.query
+            let query = {}
+            if (search) {
+                const searchQuery = {
+                    $or: [
+                        { name: { $regex: search, $options: "i" } },
+                        { email: { $regex: search, $options: "i" } }
+                    ]
+                }
+                query = { ...query, ...searchQuery }
+
+            }
+            const result = await moderatorsCollection.find(query).sort({ _id: -1 }).toArray()
+            const fakeResult = result.map((newRes) => ({ ...newRes, ...fakeDataAboutModerator }))
+
+            res.send(fakeResult)
+        })
+        // Tasks Apis  _________________________________
+
+        app.get("/tasks", async (req, res) => {
+            const { uid, search, status } = req.query
+            // console.log(email)
+            let query = { assignedTo: uid } as any
+            if (search) {
+                const searchQuery = {
+                    $or: [
+                        { name: { $regex: search, $options: "i" } },
+                        { email: { $regex: search, $options: "i" } }
+                    ]
+                }
+                query = { ...query, ...searchQuery }
+
+            }
+            if (status) {
+                query.status = status
+            }
+            const result = await tasksCollection.find(query).toArray()
+            res.send(result)
+
+        })
+        app.get("/todays_tasks", async (req, res) => {
+            const { uid } = req.query
+            const today = new Date()
+            today.setHours(0, 0, 0, 0)
+
+            const tomorrow = new Date(today)
+            tomorrow.setDate(tomorrow.getDate() + 1)
+
+            const query = {
+                assignedTo: uid,
+                createdAt: {
+                    $gte: today,
+                    $lt: tomorrow
+                }
+            }
+            const result = await tasksCollection.find(query).toArray()
+            res.send(result)
+        })
+        app.patch("/task/:id", async (req, res) => {
+            const { id } = req.params
+            const { status, postponeAt, postponeNote } = req.body
+            const query = { _id: new ObjectId(id) }
+            let update = { $set: { updatedAt: new Date() } as any, $push: {} }
+            if (status) {
+                // update = { $set: { status }, $push: {} }
+                update.$set.status = status
+            }
+            if (postponeAt) {
+                update.$push = {
+                    postpone: {
+                        postponeAt, postponeNote
+                    }
+                }
+            }
+            const result = await tasksCollection.updateOne(query, update)
+            res.send(result)
+        })
+        app.post("/task", async (req, res) => {
+            const task = req.body
+            task.status = "pending"
+            task.createdAt = new Date()
+            task.updatedAt = new Date()
+            task.photoUrl = await shareLinkToImageUrl(task.url) || ""
+            task.postpone = []
+            const result = await tasksCollection.insertOne(task)
+            res.send(result)
+        })
+        // admin 
+        app.get("/admin/clients", async (req, res) => {
+            const { search, status } = req.query
+            let query = {} as any
+            if (search) {
+                query = { name: { $regex: search, $options: "i" } }
+            }
+            if (status) {
+                query.status = status
+            }
+            const result = await tasksCollection.find(query).toArray()
+            res.send(result)
+        })
+        // special ____________________________________________
 
         app.get("/role", async (req, res) => {
             const email = req.query
